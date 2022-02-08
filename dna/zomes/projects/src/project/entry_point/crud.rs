bf8f6b4e59226dc7defc0171fba69561688f3036
Pascal Lindenau <50748295+datPascal@users.noreply.github.com>
2022-02-08 15:31:34 -0500

    Initialize 053alpha

---
+++ dna/zomes/projects/src/project/entry_point/crud.rs
@@ -0,0 +1,48 @@
+use crate::{
+    get_peers_content,
+    SignalType,
+};
+use hdk_crud::{crud, WrappedAgentPubKey, WrappedHeaderHash};
+use hdk::prelude::*;
+
+// The "Entry" in EntryPoint is not a reference to Holochain "Entries"
+// it is rather the concept of an Entrance, as in a doorway, to the tree
+#[hdk_entry(id = "entry_point")]
+#[derive(Clone, PartialEq)]
+pub struct EntryPoint {
+    pub color: String,
+    pub creator_address: WrappedAgentPubKey,
+    pub created_at: f64,
+    pub goal_address: WrappedHeaderHash,
+    pub is_imported: bool,
+}
+
+impl EntryPoint {
+    pub fn new(
+        color: String,
+        creator_address: WrappedAgentPubKey,
+        created_at: f64,
+        goal_address: WrappedHeaderHash,
+        is_imported: bool,
+      ) -> Self {
+        Self {
+          color,
+          creator_address,
+          created_at,
+          goal_address,
+          is_imported,
+        }
+    }
+}
+
+fn convert_to_receiver_signal(signal: EntryPointSignal) -> SignalType {
+    SignalType::EntryPoint(signal)
+}
+
+crud!(
+    EntryPoint,
+    entry_point,
+    "entry_point",
+    get_peers_content,
+    convert_to_receiver_signal
+);
---
+++ dna/zomes/projects/src/project/goal_comment/validate.rs
@@ -0,0 +1,87 @@
+use crate::project::validate::must_get_header_and_entry;
+use crate::project::{
+    error::Error,
+    goal_comment::crud::GoalComment,
+    validate::{
+        validate_value_matches_create_author, validate_value_matches_original_author_for_edit,
+    },
+};
+use hdk::prelude::*;
+
+#[hdk_extern]
+pub fn validate_create_entry_goal_comment(
+    validate_data: ValidateData,
+) -> ExternResult<ValidateCallbackResult> {
+    Ok(
+        // element must have an entry that must deserialize correctly
+        match GoalComment::try_from(&validate_data.element) {
+            Ok(proposed_entry) => {
+                // parent goal at goal_address must be determined to exist
+                must_get_header(proposed_entry.goal_address.0)?;
+                // an imported entry can have another listed as author, and an edit history
+                if proposed_entry.is_imported {
+                    ValidateCallbackResult::Valid
+                } else {
+                    // agent_address must match header author
+                    validate_value_matches_create_author(
+                        &proposed_entry.agent_address.0,
+                        &validate_data,
+                    )
+                }
+            }
+            Err(_e) => Error::DeserializationFailed.into(),
+        },
+    )
+}
+
+#[hdk_extern]
+pub fn validate_update_entry_goal_comment(
+    validate_data: ValidateData,
+) -> ExternResult<ValidateCallbackResult> {
+    Ok(
+        // element must have an entry that must deserialize correctly
+        match GoalComment::try_from(&validate_data.element) {
+            Ok(proposed_entry) => {
+                // agent_address must match header author
+                // and then it will also be validated that the header
+                // author is the same as the original author
+                match validate_value_matches_create_author(
+                    &proposed_entry.agent_address.0,
+                    &validate_data,
+                ) {
+                    ValidateCallbackResult::Valid => {
+                        // go a little more complicated here
+                        // -> check the original header and make sure that
+                        // this header author matches that original author
+                        if let Header::Update(header) = validate_data.element.header() {
+                            let original_goal_comment = must_get_header_and_entry::<GoalComment>(
+                                header.original_header_address.clone(),
+                            )?;
+                            // the final return value
+                            // if this passes, all have passed
+
+                            // here we are checking to make sure that
+                            // only original author can make this update
+                            validate_value_matches_original_author_for_edit(
+                                &header.author,
+                                &original_goal_comment.agent_address.0,
+                            )
+                        } else {
+                            // Holochain passed the wrong header!
+                            #[allow(unreachable_code)]
+                            return unreachable!();
+                        }
+                    }
+                    validate_callback_result => validate_callback_result,
+                }
+            }
+            Err(_e) => Error::DeserializationFailed.into(),
+        },
+    )
+}
+
+#[hdk_extern]
+/// Deletes are allowed by anyone
+pub fn validate_delete_entry_goal_comment(_: ValidateData) -> ExternResult<ValidateCallbackResult> {
+    Ok(ValidateCallbackResult::Valid)
+}
