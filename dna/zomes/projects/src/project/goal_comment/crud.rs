0a300635807ffa0f5b172a4a28b6c2bca26334a5
Pascal Lindenau <50748295+datPascal@users.noreply.github.com>
2022-02-08 15:31:35 -0500

    Initialize code-comments

---
+++ dna/zomes/projects/src/project/goal_comment/crud.rs
@@ -0,0 +1,46 @@
+use crate::{
+    get_peers_content,
+    SignalType,
+};
+use hdk_crud::{crud, WrappedAgentPubKey, WrappedHeaderHash};
+use hdk::prelude::*;
+
+#[hdk_entry(id = "goal_comment")]
+#[derive(Clone, PartialEq)]
+pub struct GoalComment {
+    pub goal_address: WrappedHeaderHash,
+    pub content: String,
+    pub agent_address: WrappedAgentPubKey,
+    pub unix_timestamp: f64,
+    pub is_imported: bool,
+}
+
+impl GoalComment {
+  pub fn new(
+      goal_address: WrappedHeaderHash,
+      content: String,
+      agent_address: WrappedAgentPubKey,
+      unix_timestamp: f64,
+      is_imported: bool,
+  ) -> Self {
+      Self {
+          goal_address,
+          content,
+          agent_address,
+          unix_timestamp,
+          is_imported,
+      }
+  }
+}
+
+fn convert_to_receiver_signal(signal: GoalCommentSignal) -> SignalType {
+    SignalType::GoalComment(signal)
+}
+
+crud!(
+    GoalComment,
+    goal_comment,
+    "goal_comment",
+    get_peers_content,
+    convert_to_receiver_signal
+);
